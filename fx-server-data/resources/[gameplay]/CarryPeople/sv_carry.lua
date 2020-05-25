5fc800f4501bdfac44fd8a2ad17047faba45029a
scktz <liam@gmx.us>
2020-05-25 22:56:54 +0100

    new carry people script

--- fx-server-data/resources/[gameplay]/CarryPeople/sv_carry.lua
+++ fx-server-data/resources/[gameplay]/CarryPeople/sv_carry.lua
@@ -1,10 +1,11 @@
-RegisterServerEvent('CarryPeople:sync')
-AddEventHandler('CarryPeople:sync', function(target, animationLib,animationLib2, animation, animation2, distans, distans2, height,targetSrc,length,spin,controlFlagSrc,controlFlagTarget,animFlagTarget)
-	TriggerClientEvent('CarryPeople:syncTarget', targetSrc, source, animationLib2, animation2, distans, distans2, height, length,spin,controlFlagTarget,animFlagTarget)
-	TriggerClientEvent('CarryPeople:syncMe', source, animationLib, animation,length,controlFlagSrc,animFlagTarget)
+  
+RegisterServerEvent('cmg2_adminations:sync')
+AddEventHandler('cmg2_animations:sync', function(target, animationLib,animationLib2, animation, animation2, distans, distans2, height,targetSrc,length,spin,controlFlagSrc,controlFlagTarget,animFlagTarget)
+	TriggerClientEvent('cmg2_animations:syncTarget', targetSrc, source, animationLib2, animation2, distans, distans2, height, length,spin,controlFlagTarget,animFlagTarget)
+	TriggerClientEvent('cmg2_animations:syncMe', source, animationLib, animation,length,controlFlagSrc,animFlagTarget)
 end)
 
-RegisterServerEvent('CarryPeople:stop')
-AddEventHandler('CarryPeople:stop', function(targetSrc)
-	TriggerClientEvent('CarryPeople:cl_stop', targetSrc)
+RegisterServerEvent('cmg2_animations:stop')
+AddEventHandler('cmg2_animations:stop', function(targetSrc)
+	TriggerClientEvent('cmg2_animations:cl_stop', targetSrc)
 end)
